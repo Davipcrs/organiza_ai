188ab7220c7eca184a801d60d195c768c5da1e38
Davipcrs <davi.coelho_comp@outlook.com>
2023-12-23 02:07:52 -0300

    note model convert grpc

--- lib/model/note.dart
+++ lib/model/note.dart
@@ -1,8 +1,38 @@
+import 'package:organiza_ai/api/generated/notes_service.pb.dart';
+
 class Note {
-  Note(this.id, this.title, this.desc, this.created, this.deadLine);
-  late int id;
-  late String title;
+  late int? id;
+  late String? title;
   late String? desc;
   late DateTime? created;
   late DateTime? deadLine;
+
+  void create(id, title, [desc, DateTime? created, DateTime? deadLine]) {
+    this.id = id;
+    this.title = title;
+    this.desc = desc;
+    if (created == null) {
+      this.created = DateTime.now();
+    } else {
+      this.created = created;
+    }
+
+    this.deadLine = deadLine;
+  }
+
+  Note? convert(NoteMessage noteMessage) {
+    Note auxiliar = Note();
+    // convert string to dateTime;
+    DateTime? auxiliarCreated;
+    DateTime? auxiliarDeadLine;
+
+    auxiliar.create(
+        id = noteMessage.id.toInt(),
+        title = noteMessage.title,
+        desc = noteMessage.desc,
+        created = auxiliarCreated,
+        deadLine = auxiliarDeadLine);
+
+    return auxiliar;
+  }
 }
